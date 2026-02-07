9958da6b3314b899f659934a19ce7626d5bf0b0a
kumakichi123 <asabe7729@gmail.com>
2026-02-07 14:28:55 +0900

    Add lavender landing page and env template

--- server.ts
+++ server.ts
@@ -27,6 +27,7 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
+app.use(express.static('public'));
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
